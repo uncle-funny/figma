071acc513cb56bdf5277700c72f8d9ec48aebf49
Oleg Petrov <funny.ltg@gmail.com>
2020-05-08 20:09:42 +0300

    add js script menu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 
 const open = document.querySelector('.burger-link');
 const openMenu = document.querySelector('.burger-menu');
+
+
+open.addEventListener('click', function(event){
+  event.stopPropagation()
+  open.classList.toggle('burger-close');
+  openMenu.classList.toggle('open-menu');
+});
+
 const body = document.querySelector('body');
 
-
-open.addEventListener('click', function(){
-  open.classList.toggle('burger-click');
-  openMenu.classList.toggle('open-menu');
+body.addEventListener('click', function(){
+  open.classList.remove('burger-close');
+  openMenu.classList.remove('open-menu');
 });
